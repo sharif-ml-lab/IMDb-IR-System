bb65f61092978a930655738ac6eae2b20dc7674c
AmirHossein-nr <arazlighi@gmail.com>
2024-02-17 19:53:46 +0330

    a sample UI that works

--- UI/main.py
+++ UI/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import sys
+sys.path.append("../")
 from Logic import utils
 import time
 from enum import Enum
@@ -13,13 +15,13 @@ class color(Enum):
 
 
 def search_time(start, end):
-    st.success(f"Search took: {end - start} seconds")
+    st.success("Search took: {:.6f} milli-seconds".format((end - start) * 1e3))
 
 
 def search_handling(
     search_button,
     search_title_terms,
-    search_abstract_terms,
+    search_summary_terms,
     search_max_num,
     search_weight,
     search_method,
@@ -27,24 +29,24 @@ def search_handling(
     if search_button:
         corrected_title = utils.correct_text(search_title_terms, utils.bigram_index)
         corrected_abstract = utils.correct_text(
-            search_abstract_terms, utils.bigram_index
+            search_summary_terms, utils.bigram_index
         )
         corrected = corrected_title + " " + corrected_abstract
 
         if (
             corrected_title != search_title_terms
-            or corrected_abstract != search_abstract_terms
+            or corrected_abstract != search_summary_terms
         ):
             st.warning(f"Your search terms were corrected to: {corrected}")
             search_title_terms = corrected_title
-            search_abstract_terms = corrected_abstract
+            search_summary_terms = corrected_abstract
 
         with st.spinner("Searching..."):
             time.sleep(0.5)  # for showing the spinner! (can be removed)
             start_time = time.time()
             result = utils.search(
                 search_title_terms,
-                search_abstract_terms,
+                search_summary_terms,
                 search_max_num,
                 search_method,
                 search_weight,
@@ -58,25 +60,25 @@ def search_handling(
 
             for i in range(len(result)):
                 card = st.columns(1)
-                info = utils.get_paper_by_id(result[i][0], utils.papers_dataset)
+                info = utils.get_movie_by_id(result[i][0], utils.movies_dataset)
                 with card[0].container():
                     st.title(info["Title"])
-                    st.markdown(f"[Link to paper]({info['URL']})")
+                    st.markdown(f"[Link to movie]({info['URL']})")
                     st.write(f"Relevance Score: {result[i][1]}")
-                    st.write(info["Abstract"])
-                    with st.expander("Authors"):
-                        num_authors = len(info["Authors"])
+                    st.write(info["Summary"])
+                    with st.expander("Cast"):
+                        num_authors = len(info["Cast"])
 
                         for j in range(num_authors):
-                            st.write(info["Authors"][j])
+                            st.write(info["Cast"][j])
 
                     topic_card = st.columns(1)
                     with topic_card[0].container():
-                        st.write("Topics:")
-                        num_topics = len(info["Related Topics"])
+                        st.write("Genres:")
+                        num_topics = len(info["Genres"])
                         for j in range(num_topics):
                             st.markdown(
-                                f"<span style='color:{random.choice(list(color)).value}'>{info['Related Topics'][j]}</span>",
+                                f"<span style='color:{random.choice(list(color)).value}'>{info['Genres'][j]}</span>",
                                 unsafe_allow_html=True,
                             )
                     st.divider()
@@ -85,15 +87,15 @@ def search_handling(
 def main():
     st.title("Search Engine")
     st.write(
-        "This is a simple search engine for academic papers. You can search through Semantic Scholar dataset and find the most relevant papers to your search terms."
+        "This is a simple search engine for IMDB movies. You can search through IMDB dataset and find the most relevant movie to your search terms."
     )
     st.markdown(
-        '<span style="color:yellow">Developed By: AmirHossein Razlighi</span>',
+        '<span style="color:yellow">Developed By: MIR Team at Sharif University</span>',
         unsafe_allow_html=True,
     )
 
     search_title_terms = st.text_input("Seacrh in title")
-    search_abstract_terms = st.text_input("Search in abstract")
+    search_summary_terms = st.text_input("Search in summary of movie")
     with st.expander("Advanced Search"):
         search_max_num = st.number_input(
             "Maximum number of results", min_value=5, max_value=100, value=10, step=5
@@ -115,7 +117,7 @@ def main():
     search_handling(
         search_button,
         search_title_terms,
-        search_abstract_terms,
+        search_summary_terms,
         search_max_num,
         search_weight,
         search_method,
