60123fc403aa980c31843ec64a661db9a706cac6
nazari02105 <X.LordAliX@yahoo.com>
2024-02-27 09:57:38 +0330

    Add test for LSH part

--- Logic/core/LSH.py
+++ Logic/core/LSH.py
@@ -1,4 +1,6 @@
 import numpy as np
+import itertools
+import random
 
 class MinHashLSH:
     def __init__(self, documents, num_hashes):
@@ -90,3 +92,44 @@ class MinHashLSH:
         """
          # TODO
         return
+
+    def jaccard_score(self, first_set, second_set):
+        return len(first_set.intersection(second_set)) / len(first_set.union(second_set))
+
+    def jaccard_similarity_test(self, buckets, all_documents):
+        correct_near_duplicates = 0
+        all_near_duplicates = 0
+
+        for bucket_id in buckets.keys():
+            docs_in_this_bucket = buckets[bucket_id]
+            unique_doc_ids = set(docs_in_this_bucket)
+            if len(unique_doc_ids) > 1:
+                combinations = list(itertools.combinations(unique_doc_ids, 2))
+                for comb in combinations:
+                    all_near_duplicates += 1
+                    
+                    first_doc_id = comb[0]
+                    second_doc_id = comb[1]
+                    
+                    first_shingled_doc = self.shingle_document(all_documents[first_doc_id], 2)
+                    second_shingled_doc = self.shingle_document(all_documents[second_doc_id], 2)
+                    
+                    near_duplicated_jaccard_score = self.jaccard_score(first_shingled_doc, second_shingled_doc)
+                    current_score = 0
+                    
+                    for _ in range(5):
+                        random_doc_id = first_doc_id
+                        while random_doc_id == first_doc_id or random_doc_id == second_doc_id:
+                            random_doc_id = random.randint(0, len(all_documents)-1)
+                        random_shingled_doc = self.shingle_document(all_documents[random_doc_id], 2)
+                        
+                        random_jaccard_score = self.jaccard_score(first_shingled_doc, random_shingled_doc)
+                        
+                        if near_duplicated_jaccard_score > random_jaccard_score:
+                            current_score += 1
+                            
+                    if current_score == 5:
+                        correct_near_duplicates += 1
+                        
+        # a good score is around 0.8
+        print("your final score in near duplicate detection:", correct_near_duplicates / all_near_duplicates)
