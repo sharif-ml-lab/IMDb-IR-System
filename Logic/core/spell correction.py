53a7632c545af07e667d20e072600b0cbb0c8c02
nazari02105 <X.LordAliX@yahoo.com>
2024-03-11 07:00:15 +0330

    Add snippet and spell correction questions

---
+++ Logic/core/spell correction.py
@@ -0,0 +1,117 @@
+class SpellCorrection:
+    def __init__(self, all_documents):
+        """
+        Initialize the SpellCorrection
+
+        Parameters
+        ----------
+        all_documents : list of str
+            The input documents.
+        """
+        self.all_shingled_words, self.word_counter = self.shingling_and_counting(all_documents)
+
+    def shingle_word(self, word, k=2):
+        """
+        Convert a word into a set of shingles.
+
+        Parameters
+        ----------
+        word : str
+            The input word.
+        k : int
+            The size of each shingle.
+
+        Returns
+        -------
+        set
+            A set of shingles.
+        """
+        shingles = set()
+        
+        # TODO: Create shingle here
+
+        return shingles
+    
+    def jaccard_score(self, first_set, second_set):
+        """
+        Calculate jaccard score.
+
+        Parameters
+        ----------
+        first_set : set
+            First set of shingles.
+        second_set : set
+            Second set of shingles.
+
+        Returns
+        -------
+        float
+            Jaccard score.
+        """
+
+        # TODO: Calculate jaccard score here.
+
+        return
+
+    def shingling_and_counting(self, all_documents):
+        """
+        Shingle all words of the corpus and count TF of each word.
+
+        Parameters
+        ----------
+        all_documents : list of str
+            The input documents.
+
+        Returns
+        -------
+        all_shingled_words : dict
+            A dictionary from words to their shingle sets.
+        word_counter : dict
+            A dictionary from words to their TFs.
+        """
+        all_shingled_words = dict()
+        word_counter = dict()
+
+        # TODO: Create shingled words dictionary and word counter dictionary here.
+                
+        return all_shingled_words, word_counter
+    
+    def find_nearest_words(self, word):
+        """
+        Find correct form of a misspelled word.
+
+        Parameters
+        ----------
+        word : stf
+            The misspelled word.
+
+        Returns
+        -------
+        list of str
+            5 nearest words.
+        """
+        top5_candidates = list()
+
+        # TODO: Find 5 nearest candidates here.
+
+        return top5_candidates
+    
+    def spell_check(self, query):
+        """
+        Find correct form of a misspelled query.
+
+        Parameters
+        ----------
+        query : stf
+            The misspelled query.
+
+        Returns
+        -------
+        str
+            Correct form of the query.
+        """
+        final_result = ""
+        
+        # TODO: Do spell correction here.
+
+        return final_result
