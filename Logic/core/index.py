e00d2492a54b0bfc5e0edb4470cf8a31637a9205
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-03-15 13:52:27 +0330

    Change method input type

--- Logic/core/index.py
+++ Logic/core/index.py
@@ -183,7 +183,7 @@ class Index:
         else:
             print('Remove is incorrect')
 
-    def store_index(self, path: str, index_type: str):
+    def store_index(self, path: str, index_type: str = None):
         """
         Stores the index in a file (such as a JSON file)
 
