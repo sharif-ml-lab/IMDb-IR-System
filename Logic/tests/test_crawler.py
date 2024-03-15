db38e774e0cddf8b64124ef044e3dbef66fe43ac
AmirHosein Rahmati <amirhossein@smartech.ir>
2024-03-15 18:20:31 +0330

    fix_crawler_test_bug
    fix_crawler_test_bug

--- Logic/tests/test_crawler.py
+++ Logic/tests/test_crawler.py
@@ -5,19 +5,29 @@ from typing import List
 def check_field_types(json_file_path, expected_fields):
     with open(json_file_path, "r") as file:
         data = json.load(file)
+
+
     # check len of the data
-    assert len(data) > 500, f"Expected at least 1000 movies, but got {len(data)}"
+    assert len(data) > 1000, f"Expected at least 1000 movies, but got {len(data)}"
 
     # check data types
     for movie in data:
         for field, expected_type in expected_fields.items():
-            assert (
-                field in movie
-            ), f'Expected field {field} not found in movie {movie["id"]}'
-            if expected_type is not None:
-                assert isinstance(
-                    movie[field], expected_type
-                ), f'Expected field {field} to be of type {expected_type}, but got {type(movie[field])} in movie {movie["id"]}'
+            if field not in movie or movie[field] is None:
+                print(f'Expected field {field} not found in movie {movie["id"]}')
+            else:    
+                if expected_type is not None:
+                    if expected_type == str:    
+                        assert isinstance(
+                            movie[field], expected_type
+                        ), f'Expected field {field} to be of type {expected_type}, but got {type(movie[field])} in movie {movie["id"]}'
+                    if expected_type == List[str]:
+                        assert isinstance(
+                            movie[field], list
+                        ), f'Expected field {field} to be of type {expected_type}, but got {type(movie[field])} in movie {movie["id"]}'
+                        assert all(
+                            isinstance(x, str) for x in movie[field]
+                        ), f'Expected field {field} to be of type {expected_type}, but got {type(movie[field])} in movie {movie["id"]}'
 
 
 expected_fields = {
