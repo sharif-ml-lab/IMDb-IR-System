69d73860d8f33171b376ab4b557b6ff2acc5d78f
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-03-15 13:42:25 +0330

    Change comment for get_documents_length method in get_document_lengths_index_answer.py

--- Logic/core/document_lengths_index.py
+++ Logic/core/document_lengths_index.py
@@ -15,16 +15,25 @@ class DocumentLengthsIndex:
 
         self.documents_index = documents_index
         self.document_length_index = {
-            Indexes.STARS.value: self.get_document_lengths(Indexes.STARS.value),
-            Indexes.GENRES.value: self.get_document_lengths(Indexes.GENRES.value),
-            Indexes.SUMMARIES.value: self.get_document_lengths(Indexes.SUMMARIES.value)
+            Indexes.STARS.value: self.get_documents_length(Indexes.STARS.value),
+            Indexes.GENRES.value: self.get_documents_length(Indexes.GENRES.value),
+            Indexes.SUMMARIES.value: self.get_documents_length(Indexes.SUMMARIES.value)
         }
 
-    def get_document_lengths(self, where):
+    def get_documents_length(self, where):
         """
-        Returns the length of a document in a specific index.
+        Gets the documents' length for the specified field.
 
+        Parameters
+        ----------
+        where : str
+            The field to get the document lengths for.
 
+        Returns
+        -------
+        dict
+            A dictionary of the document lengths. The keys are the document IDs, and the values are
+            the document's length in that field (where).
         """
 
         # TODO: Implement this method
