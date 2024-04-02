fc768739e0236d1bee5a5b02a3be5b23d2711319
AmirHossein-nr <arazlighi@gmail.com>
2024-04-02 23:50:17 +0330

    tiered_index update

--- Logic/core/indexer/index.py
+++ Logic/core/indexer/index.py
@@ -183,7 +183,7 @@ class Index:
         else:
             print('Remove is incorrect')
 
-    def store_index(self, path: str, index_type: str = None):
+    def store_index(self, path: str, index_name: str = None):
         """
         Stores the index in a file (such as a JSON file)
 
@@ -191,22 +191,17 @@ class Index:
         ----------
         path : str
             Path to store the file
-        index_type: str or None
-            type of index we want to store (documents, stars, genres, summaries)
-            if None store tiered index
+        index_name: str
+            name of index we want to store (documents, stars, genres, summaries)
         """
 
         if not os.path.exists(path):
             os.makedirs(path)
 
-        if index_type is None:
-            # TODO
-            pass
+        if index_name not in self.index:
+            raise ValueError('Invalid index name')
 
-        if index_type not in self.index:
-            raise ValueError('Invalid index type')
-
-        #         TODO
+        # TODO
         pass
 
     def load_index(self, path: str):
