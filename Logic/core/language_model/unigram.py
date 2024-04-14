eaad01323e8ebb378a8c22ca776f06fcf78d00bd
AmirHossein-nr <arazlighi@gmail.com>
2024-04-14 19:02:17 +0330

    update comments for plotting

--- Logic/core/language_model/unigram.py
+++ Logic/core/language_model/unigram.py
@@ -7,32 +7,36 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from tokenizer import Tokenizer
 
-nltk.download('punkt')
+nltk.download("punkt")
+
+
 def set_style(plt):
-    plt.style.use('seaborn')
-    plt.rcParams['legend.numpoints'] = 1
-    plt.rcParams['axes.labelsize'] = 20
-    plt.rcParams['axes.titlesize'] = 20
-    plt.rcParams['xtick.labelsize'] = 15
-    plt.rcParams['ytick.labelsize'] = 15
-    plt.rcParams['legend.fontsize'] = 10
+    plt.style.use("seaborn")
+    plt.rcParams["legend.numpoints"] = 1
+    plt.rcParams["axes.labelsize"] = 20
+    plt.rcParams["axes.titlesize"] = 20
+    plt.rcParams["xtick.labelsize"] = 15
+    plt.rcParams["ytick.labelsize"] = 15
+    plt.rcParams["legend.fontsize"] = 10
     return plt
 
 
 legend_opts = {
-    'fontsize': 15,
-    'frameon': True,
-    'framealpha': 1,
-    'facecolor': 'white',
-    'edgecolor': 'black',
-    'labelspacing': 0.1}
+    "fontsize": 15,
+    "frameon": True,
+    "framealpha": 1,
+    "facecolor": "white",
+    "edgecolor": "black",
+    "labelspacing": 0.1,
+}
 
 
 def savefig(fig, filenaشme, **kwargs):
-    fig.savefig(f'../viz/{filename}', bbox_inches='tight', **kwargs)
+    fig.savefig(f"../viz/{filename}", bbox_inches="tight", **kwargs)
 
 
 plt = set_style(plt)
+
 
 class UnigramCounter:
     def __init__(self, file_name: str, tokenizer: Tokenizer) -> None:
@@ -68,7 +72,7 @@ class UnigramModel:
         """
         self.counter = train_counter
         self.counts = train_counter.counts.copy()
-        self.counts['[UNK]'] = 0
+        self.counts["[UNK]"] = 0
         self.vocab = set(self.counts.keys())
         self.vocab_size = len(self.vocab)
 
@@ -100,11 +104,14 @@ class UnigramModel:
         # TODO
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO: You should do these steps:
     # 1. Load the dataset (e.g. using pd.read_csv)
     # 2. Tokenize the dataset properly (using your Tokenizer class)
     # 3. Create `UnigramCounter` and `UnigramModel` for your data
     # 4. Create model and train
-    # 5. ... To be Completed ...
+    # 5. Plot the results of your code. show the most and least commnon words.
+    # 6. show the words ditstrubution.
+    # 7. show each result with or without smoothing
+    # IMPORTANT NOTE: for plotting, you should use Wandb (You will need this for the next parts and phases, too!)
     pass
