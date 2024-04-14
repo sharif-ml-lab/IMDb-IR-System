ed009329067312b883c54df73158e893348252b6
AmirHossein-nr <arazlighi@gmail.com>
2024-04-14 09:26:34 +0330

    Update repo for first release of phase 2

--- Logic/utils.py
+++ Logic/utils.py
@@ -1,8 +1,8 @@
 from typing import Dict, List
 from core.search import SearchEngine
-from core.spell_correction import SpellCorrection
-from core.snippet import Snippet
-from core.indexes_enum import Indexes, Index_types
+from core.utility.spell_correction import SpellCorrection
+from core.utility.snippet import Snippet
+from core.indexer.indexes_enum import Indexes, Index_types
 import json
 
 movies_dataset = None  # TODO: load your movies dataset (from the json file you saved your indexes in), here
